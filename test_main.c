8e491692039d6a33bb0da473be114de54f3cdfbd
mreynaud42 <mathisreynaud07@gmail.com>
2026-03-08 21:19:38 +0100

    add: code test strcpy and check error

--- test_main.c
+++ test_main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+int nb_error = 0;
 
 size_t ft_strlen(const char *s);
 
 void test_str_strlen(const char *s)
 {
-    if (strlen(s) < 80) {
+    if (strlen(s) != ft_strlen(s)) {
+        nb_error++;
+        printf("\033[31m   strlen(\"%s\") = %ld \n", s, strlen(s));
+        printf("ft_strlen(\"%s\") = %ld \033[0m \n\n", s, ft_strlen(s));
+    }
+    else if (strlen(s) < 80) {
         printf("   strlen(\"%s\") = %ld\n", s, strlen(s));
         printf("ft_strlen(\"%s\") = %ld\n\n", s, ft_strlen(s));
     } else {
@@ -18,7 +26,7 @@ void test_strlen()
 {
     printf("#################### strlen ####################\n");
     {
-        char *s = "Hello World!";
+        const char *s = "Hello World!";
         test_str_strlen(s);
     }
     {
@@ -40,9 +48,77 @@ void test_strlen()
     }
 }
 
+
+char *ft_strcpy(char *dest, const char *src);
+
+void test_str_strcpy(char *dest, const char *src)
+{
+    char *strA = strcpy(dest, src);
+    char *strB = ft_strcpy(dest, src);
+    if (strcmp(strA, strB) == 0)
+        printf("strcmp(strcpy, ft_strcpy) = %d\n\n", strcmp(strA, strB));
+    else
+    {
+        nb_error++;
+        printf("\033[31m strcmp(strcpy, ft_strcpy) = %d \033[0m\n\n", strcmp(strA, strB));
+        printf("   strcpy = \"%s\"\n", strA);
+        printf("ft_strcpy = \"%s\"\n", strB);
+    }
+}
+
+void test_strcpy()
+{
+    printf("#################### strcpy ####################\n");
+    {
+        char dest[100];
+        const char *src = "Hello World!";
+
+        test_str_strcpy((char *)dest, src);
+    }
+    {
+        char dest[0];
+        test_str_strcpy((char *)dest, "");
+    }
+    {
+        const char *src = "ijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgrea";
+        char *dest = malloc((strlen(src) + 1) * sizeof(char));
+        if (dest)
+        {
+            test_str_strcpy(dest, src);
+            free(dest);
+        }
+    }
+    {
+        char dest[100] = "The strings may not overlap\0";
+        const char *src = dest + 5;
+
+        test_str_strcpy(dest, src);
+    }
+    {
+        // char src[100] = "The strings may not overlap\0"; // segfault
+
+        // test_str_strcpy(src + 2, src);
+    }
+    {
+        // const char *src = "Hey"; // segfault
+        // char *dest;
+
+        // test_str_strcpy(dest, src);
+    }
+    {
+        // test_str_strcpy((const char *)0, (const char *)0); // segfault
+    }
+}
+
 int main()
 {
     test_strlen();
+    test_strcpy();
     
+    if (nb_error != 0) {
+        printf("\033[31m ##### ##### ##### ##### ##### \n");
+        printf(" #####      ERROR: %d \t ##### \n", nb_error);
+        printf(" ##### ##### ##### ##### ##### \033[0m \n");
+    }
     return 0;
 }
