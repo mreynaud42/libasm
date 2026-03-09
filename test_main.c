9d640131db7046270bdeb69e524773325db90ab6
mreynaud42 <mathisreynaud07@gmail.com>
2026-03-09 22:38:49 +0100

    add: test for ft_strcmp

--- test_main.c
+++ test_main.c
@@ -13,7 +13,7 @@ void test_str_strlen(const char *s)
         printf("\033[31m   strlen(\"%s\") = %ld \n", s, strlen(s));
         printf("ft_strlen(\"%s\") = %ld \033[0m \n\n", s, ft_strlen(s));
     }
-    else if (strlen(s) < 80) {
+    else if (strlen(s) < 40) {
         printf("   strlen(\"%s\") = %ld\n", s, strlen(s));
         printf("ft_strlen(\"%s\") = %ld\n\n", s, ft_strlen(s));
     } else {
@@ -110,10 +110,74 @@ void test_strcpy()
     }
 }
 
+
+int ft_strcmp(const char *s1, const char *s2);
+
+void test_str_strcmp(const char *s1, const char *s2)
+{
+    if (strcmp(s1, s2) != ft_strcmp(s1, s2)) {
+        nb_error++;
+        printf("\033[31m   strcmp(\"%s\", \"%s\") = %d \n", s1, s2, strcmp(s1, s2));
+        printf("ft_strcmp(\"%s\", \"%s\") = %d \033[0m \n\n", s1, s2, ft_strcmp(s1, s2));
+    }
+    else if (strlen(s1) < 20 || strlen(s2) < 20) {
+        printf("   strcmp(\"%s\", \"%s\") = %d\n", s1, s2, strcmp(s1, s2));
+        printf("ft_strcmp(\"%s\", \"%s\") = %d\n\n", s1, s2, ft_strcmp(s1, s2));
+    } else {
+        printf("   strcmp(\"...\", \"...\") = %d\n", strcmp(s1, s2));
+        printf("ft_strcmp(\"...\", \"...\") = %d\n\n", ft_strcmp(s1, s2));
+    }
+}
+
+void test_strcmp()
+{
+    printf("#################### strcmp ####################\n");
+    {
+        const char *s1 = "Hello World!";
+        const char *s2 = "Hello World!";
+        test_str_strcmp(s1, s2);
+    }
+    {
+        test_str_strcmp("", "");
+    }
+    {
+        char *s1 = "ijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgreaijwgbwjhbgjwnljgbrjgbrkjgbabrgbakjrgbrkgbkjaerbjgbrajbgrbekagjkrbejkeaberkjblkgarjebagjbakjjgrkbgekjabrgajkbgkjbgarjjkbbrklbgrerjgbaekjabgabrjgrea";
+        test_str_strcmp(s1, s1);
+    }
+    {
+        const char *s1 = "Hello World!";
+        const char *s2 = "Helll World!";
+        test_str_strcmp(s1, s2);
+        test_str_strcmp(s2, s1);
+    }
+    {
+        char dest[100] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\0";
+        const char *src = dest + 5;
+
+        test_str_strcmp(dest, src);
+    }
+    {
+        const char *s1 = "\x0";
+        const char *s2 = "\x7f";
+        test_str_strcmp(s1, s2);
+        test_str_strcmp(s2, s1);
+    }
+    {
+        const char *s1 = "\x0";
+        const char *s2 = "\xff";
+        test_str_strcmp(s1, s2);
+        test_str_strcmp(s2, s1);
+    }
+    {
+        // test_str_strcmp((const char *)0, (const char *)0); // segfault
+    }
+}
+
 int main()
 {
     test_strlen();
     test_strcpy();
+    test_strcmp();
     
     if (nb_error != 0) {
         printf("\033[31m ##### ##### ##### ##### ##### \n");
