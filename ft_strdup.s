7a4824acaceb7d75a2de3c403777e5f4d15bfe88
mreynaud42 <mathisreynaud07@gmail.com>
2026-03-11 17:23:16 +0100

    add: code strdup

---
+++ ft_strdup.s
@@ -0,0 +1,33 @@
+
+; char *ft_strdup(const char *s);
+; rax ft_strdup rdi
+
+; rdi rsi rdx rcx r8 r9
+
+extern __errno_location
+extern malloc
+extern ft_strlen
+extern ft_strcpy
+
+section .text
+    global ft_strdup
+
+ft_strdup:
+    call ft_strlen
+
+    inc rax         ; add 1 for \0
+    mov r12, rdi    ; save rdi in r12
+    mov rdi, rax
+    call malloc wrt ..plt
+    jnz .ok
+
+    
+    .error:         ; if errno=ENOMEM
+        ret
+
+    .ok:
+        mov rdi, rax
+        mov rsi, r12
+        call ft_strcpy
+
+        ret
