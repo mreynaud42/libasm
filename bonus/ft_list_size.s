26a137d4008fac1d48a2313ef870368a871368bc
mreynaud42 <mathisreynaud07@gmail.com>
2026-03-18 14:56:36 +0100

    add: ft_list_size

--- bonus/ft_list_size.s
+++ bonus/ft_list_size.s
@@ -1,2 +1,26 @@
 
 ; int ft_list_size(t_list *begin_list);
+
+struc   node
+    .data:  resq 1
+    .next:  resq 1
+endstruc
+
+section .text
+    global ft_list_size
+
+ft_list_size:
+    ; rdi = t_list *begin_list
+
+    mov rax, 0
+
+    ._loop:
+        test rdi, rdi
+        jz .end
+
+        inc rax
+        mov rdi, [rdi + node.next]
+        jmp ._loop
+
+    .end:
+        ret
